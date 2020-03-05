aac9369715ae249c3ce260c13c61e5a018d5d828
Fedee <fedegomezavalos@gmail.com>
2020-03-05 17:59:58 -0300

    Prueba de video

--- js/test.js
+++ js/test.js
@@ -3,16 +3,51 @@ btnRecord.addEventListener("click", event => {
     event.target;
     getStreamAndRecord();
 })
-function getStreamAndRecord() {
-    navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-            height: { max: 480 }
-        }
-    })
-        .then(function (stream) {
-            video.srcObject = stream;
-            video.play();
+const video = document.querySelector('video');
 
-        })
+async function stopRecordingCallback() {
+    video.srcObject = null;
+    let blob = await recorder.getBlob();
+    video.src = URL.createObjectURL(blob);
+    recorder.stream.getTracks(t => t.stop());
+
+    // reset recorder's state
+    await recorder.reset();
+
+    // clear the memory
+    await recorder.destroy();
+
+    // so that we can record again
+    recorder = null;
 }
+
+let recorder; // globally accessible
+
+document.getElementById('btn-start-recording').onclick = async function() {
+    this.disabled = true;
+    let stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+    video.srcObject = stream;
+    recorder = new RecordRTCPromisesHandler(stream, {
+        type: 'video'
+    });
+    await recorder.startRecording();
+    
+    // helps releasing camera on stopRecording
+    recorder.stream = stream;
+
+    document.getElementById('btn-stop-recording').disabled = false;
+
+    // if you want to access internal recorder
+    const internalRecorder = await recorder.getInternalRecorder();
+    console.log('internal-recorder', internalRecorder.name);
+
+    // if you want to read recorder's state
+    console.log('recorder state: ', await recorder.getState());
+};
+
+document.getElementById('btn-stop-recording').onclick = async function() {
+    this.disabled = true;
+    await recorder.stopRecording();
+    stopRecordingCallback();
+    document.getElementById('btn-start-recording').disabled = false;
+};
