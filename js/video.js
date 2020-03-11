8cb14b24e37d58f1981e5546504033d20676a8a8
fedemga <fedegomezavalos@gmail.com>
2020-03-11 17:57:46 -0300

    Avanzando con el upload

--- js/video.js
+++ js/video.js
@@ -5,57 +5,31 @@ async function stopRecordingCallback() {
   let blob = await recorder.getBlob();
   video.src = URL.createObjectURL(blob);
   recorder.stream.getTracks(t => t.stop());
-  // reset recorder's state
+  /* // reset recorder's state
   await recorder.reset();
   // clear the memory
   await recorder.destroy();
   // so that we can record again
-  recorder = null;
+  recorder = null; */
+  document.getElementById("upload").addEventListener("click", () => {
+    const formData = new FormData();
+    formData.append("username","federicogomezavalos")
+    formData.append("file", blob, "myGif.gif");
+    console.log(formData.get("file"));
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", uploadURL, true);
+    xhr.withCredentials = true;
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xhr.onreadystatechange = function() {
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        const gifObject = JSON.parse(xhr.response);
+        xhr.send(formData);
+      }
+    };
+  });
 }
 
 document.getElementById("btn-start-recording").addEventListener("click", async function() {
-  videoLayout();
-  this.disabled = true;
-  let stream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: false
-  });
-  video.srcObject = stream;
-  recorder = new RecordRTCPromisesHandler(stream, {
-    type: "video",
-    frameRate: 1,
-    quality: 10,
-    width: 360,
-    hidden: 240
-  });
-  document.getElementById("btn-record").addEventListener("click", async function(){
-   midStep();
-    await recorder.startRecording();
-    // helps releasing camera on stopRecording
-    recorder.stream = stream;
-    document.getElementById("btn-stop-recording").disabled = false;
-    // if you want to access internal recorder
-    const internalRecorder = await recorder.getInternalRecorder();
-    console.log("internal-recorder", internalRecorder.name);
-    // if you want to read recorder's state
-    console.log("recorder state: ", await recorder.getState());
-    document.getElementById("btn-save").addEventListener("click", async function() {
-      await recorder.stopRecording();
-      recorder.stream.getTracks(t => t.stop());
-      finalStep();
-    });
-
- /*    document.getElementById("btn-stop-recording").addEventListener("click", async function() {
-        menuLayout()
-        this.disabled = true;
-        await recorder.stopRecording();
-        stopRecordingCallback();
-        document.getElementById("btn-start-recording").disabled = false;
-      }); */
-  })
-})
-
-/* document.getElementById("btn-start-recording").addEventListener("click", async function() {
     videoLayout();
     this.disabled = true;
     let stream = await navigator.mediaDevices.getUserMedia({
@@ -70,45 +44,46 @@ document.getElementById("btn-start-recording").addEventListener("click", async f
       width: 360,
       hidden: 240
     });
-    await recorder.startRecording();
-    // helps releasing camera on stopRecording
-    recorder.stream = stream;
-    document.getElementById("btn-stop-recording").disabled = false;
-    // if you want to access internal recorder
-    const internalRecorder = await recorder.getInternalRecorder();
-    console.log("internal-recorder", internalRecorder.name);
-    // if you want to read recorder's state
-    console.log("recorder state: ", await recorder.getState());
-    document.getElementById("btn-stop-recording").addEventListener("click", async function() {
-        menuLayout()
-        this.disabled = true;
-        await recorder.stopRecording();
-        stopRecordingCallback();
-        document.getElementById("btn-start-recording").disabled = false;
+    document.getElementById("btn-record").addEventListener("click", async function() {
+        midStep();
+        await recorder.startRecording();
+        // helps releasing camera on stopRecording
+        recorder.stream = stream;
+        document.getElementById("btn-stop-recording").disabled = false;
+        // if you want to access internal recorder
+        const internalRecorder = await recorder.getInternalRecorder();
+        console.log("internal-recorder", internalRecorder.name);
+        // if you want to read recorder's state
+        console.log("recorder state: ", await recorder.getState());
+        document.getElementById("btn-save").addEventListener("click", async function() {
+            await recorder.stopRecording();
+            recorder.stream.getTracks(t => t.stop());
+            finalStep();
+          });
       });
   });
- */
-  let all;
+
+let all;
 document.getElementById("dark-theme").addEventListener("click", function classToggle() {
-  all = document.getElementsByTagName("*");
-  for (let i = 0; i < all.length; i++) {
-    all[i].classList.add("dark");
-  }
-  document.getElementById("logo").src = "/img/gifOF_logo_dark.png";
-});
+    all = document.getElementsByTagName("*");
+    for (let i = 0; i < all.length; i++) {
+      all[i].classList.add("dark");
+    }
+    document.getElementById("logo").src = "/img/gifOF_logo_dark.png";
+  });
 document.getElementById("light-theme").addEventListener("click", function classDelete() {
-  all = document.getElementsByTagName("*");
-  for (let i = 0; i < all.length; i++) {
-    all[i].classList.remove("dark");
-  }
-  document.getElementById("logo").src = "/img/gifOF_logo.png";
-});
+    all = document.getElementsByTagName("*");
+    for (let i = 0; i < all.length; i++) {
+      all[i].classList.remove("dark");
+    }
+    document.getElementById("logo").src = "/img/gifOF_logo.png";
+  });
 
 function videoLayout() {
   document.getElementById("video").classList.remove("hide");
   document.getElementById("text-box").classList.add("hide");
   document.getElementById("myGifos-bar").classList.add("hide");
-  document.getElementById("window-txt").innerHTML="Un Chequeo Antes de Empezar";
+  document.getElementById("window-txt").innerHTML ="Un Chequeo Antes de Empezar";
   document.getElementById("windowContainer").classList.remove("square-windows");
   document.getElementById("windowContainer").classList.add("newSize");
   document.getElementById("btn-record").classList.add("btn-flex");
@@ -119,16 +94,18 @@ function menuLayout() {
   document.getElementById("text-box").classList.remove("hide");
   document.getElementById("myGifos-bar").classList.remove("hide");
 }
-function midStep(){  
+function midStep() {
   document.getElementById("btn-record").classList.remove("btn-flex");
   document.getElementById("btn-record").classList.add("hide");
   document.getElementById("btn-save").classList.remove("hide");
-  document.getElementById("btn-save").classList.add("flex");
+  document.getElementById("btn-save").classList.add("btn-flex");
 }
 function finalStep() {
+  document.getElementById("btn-save").classList.remove("btn-flex");
   document.getElementById("btn-save").classList.add("hide");
   document.getElementById("final-step").classList.remove("hide");
-  document.getElementById("final-step").classList.add("flex")
+  document.getElementById("final-step").classList.add("btn-flex");
   stopRecordingCallback();
-  
 }
+
+const uploadURL ="http://upload.giphy.com/v1/gifs?api_key=5k0ncuBQ9e0JQau3FauPqVrzbWfJiqqR";
